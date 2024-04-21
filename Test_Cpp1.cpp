ab5283cdb320a29a6d132b81bf81c9b3a7ad9da1
AdolinZhuch <ndt-tomsk@yandex.ru>
2024-04-21 18:28:35 +0700

    Изменение 2

--- Test_Cpp1.cpp
+++ Test_Cpp1.cpp
@@ -6,7 +6,7 @@ int main() {
   // BEGIN (write your solution here)
   std::cout << "Hello, World!"<< std::endl;
   std::cin >> wast;
-    if(wast >=0 ) std::cout << "wast = 0"<< std::endl;
+    if(wast <=0 ) std::cout << "wast = 0"<< std::endl;
     else std::cout << "wast !=0"<< std::endl;
   // END
   return 0;
